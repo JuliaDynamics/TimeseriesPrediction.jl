43012946732d39a3ff4dbd6bdca0f9dfd255104e
Jonas Isensee <jonas.isensee@web.de>
2018-03-05 10:02:25 +0100

    Added Test for MSEp

--- test/localmodeling_tests.jl
+++ test/localmodeling_tests.jl
@@ -9,7 +9,7 @@ ds = Systems.roessler()
 data = trajectory(ds, 210;dt=0.01)
 N_train = 20000
 s_train = data[1:N_train, 1];
-s_test  = data[N_train:N_train+200,1]
+s_test  = data[N_train:end,1]
 
 @testset "TSP" begin
     @testset "D=$D and τ=$τ" for D ∈ [3,4], τ ∈ [50,60]
@@ -22,5 +22,22 @@ s_test  = data[N_train:N_train+200,1]
         s_pred = TSP(tree,R,num_points,LocalModel,method,f)
         @test length(s_pred) == num_points+1
         @test norm(s_test[1:num_points+1] - s_pred)/num_points < 5e-2
+    end
+end
+
+
+
+println("Testing MSEp")
+
+@testset "MSEp" begin
+    @testset "p=$p" for p ∈ [50,100]
+        D = 3; τ = 50;
+        R = Reconstruction(s_train,D,τ)
+        tree = KDTree(R)
+        R_test = Reconstruction(s_test[end-D*τ-p-50:end],D,τ)
+        LocalModel = LocalAverageModel(2)
+        method = FixedMassNeighborhood(5)
+        f(i) = i+1
+        @test MSEp(tree,R,R_test,p,LocalModel,method,f) < 5e-2
     end
 end
