e6defc07c1f07a4e29aab12fd817766534fc49c9
Jonas Isensee <jonas.isensee@web.de>
2018-03-09 10:22:35 +0100

    changed tests accordingly

--- test/localmodeling_tests.jl
+++ test/localmodeling_tests.jl
@@ -11,16 +11,23 @@ N_train = 40000
 s_train = data[1:N_train, 1]
 s_test  = data[N_train:end,1]
 
-@testset "predict" begin
+@testset "predict_timeseries" begin
     @testset "D=$D and τ=$τ" for D ∈ [3,4], τ ∈ [140,150]
-        R = Reconstruction(s_train,D,τ)
-        num_points = 50
-        LocalModel = LocalAverageModel(2)
-        method = FixedMassNeighborhood(2)
-        f(i) = i+1
-        s_pred = predict(R,num_points,LocalModel,method,f)
-        @test length(s_pred) == num_points+1
-        @test norm(s_test[1:num_points+1] - s_pred)/num_points < 5e-2
+        p = 50
+        method = LocalAverageModel(2)
+        ntype = FixedMassNeighborhood(2)
+        step = 1
+        s_pred = predict_timeseries(s_train, D, τ, p;
+         method=method, ntype=ntype, step=step)
+        @test length(s_pred) == p+1
+        @test norm(s_test[1:p+1] - s_pred)/p < 5e-2
+
+        #Repeat with reconstruction given
+        R = Reconstruction(s_train, D, τ)
+        s_pred = predict_timeseries(R, p; method=method, ntype=ntype, step=step)
+        @test length(s_pred) == p+1
+        @test norm(s_test[1:p+1] - s_pred)/p < 5e-2
+
     end
 end
 
@@ -30,10 +37,10 @@ end
     @testset "D=$D and τ=$τ" for D ∈ [3,4], τ ∈ [140,150]
         R = Reconstruction(sm_train,D,τ)
         num_points = 50
-        LocalModel = LocalAverageModel(2)
-        method = FixedMassNeighborhood(2)
-        f(i) = i+1
-        s_pred = predict(R,num_points,LocalModel,method,f)
+        method = LocalAverageModel(2)
+        ntype = FixedMassNeighborhood(2)
+        step = 1
+        s_pred = predict_timeseries(R,num_points; method=method,ntype=ntype,step=step)
         @test length(s_pred) == num_points+1
         @test norm(s_test[1:num_points+1] - s_pred)/num_points < 5e-2
     end
@@ -49,10 +56,10 @@ println("Testing MSEp")
         R = Reconstruction(s_train,D,τ)
         tree = KDTree(R)
         R_test = Reconstruction(s_test[end-D*τ-p-50:end],D,τ)
-        LocalModel = LocalAverageModel(2)
-        method = FixedMassNeighborhood(2)
-        f(i) = i+1
-        @test MSEp(R,R_test,p,LocalModel,method,f) < 5e-2
-        @test MSE1(R,R_test,LocalModel,method,f) < 5e-2
+        method = LocalAverageModel(2)
+        ntype = FixedMassNeighborhood(2)
+        step = 1
+        @test MSEp(R,R_test,p; method=method,ntype=ntype,step=step) < 5e-2
+        @test MSE1(R,R_test,; method=method,ntype=ntype,step=step) < 5e-2
     end
 end
