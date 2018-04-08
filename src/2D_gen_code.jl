96078ff7d8297ddb87a26a8e389999ae393240d9
Jonas Isensee <jonas.isensee@web.de>
2018-04-08 11:19:09 +0200

    neat

--- src/2D_gen_code.jl
+++ src/2D_gen_code.jl
@@ -44,30 +44,31 @@ function my_reconstruct_impl(::Val{D}, ::Val{B}) where {D, B}
     gens = [:(0 < mx + $i<=X && 0 < my + $j <= Y ?
      s[mx + $i, my + $j, t + $d*τ] : boundary)
       for i=-B:B, j=-B:B, d=0:D-1]
-
     quote
         X,Y,L = size(s)
         L -= $(D-1)*τ
         T = eltype(s)
-        data = Vector{SVector{$D*(2*$B + 1)^2, T}}(L*X*Y)
+        data = Vector{SVector{$D*(2*$B + 1)^2+2, T}}(L*X*Y)
         for my ∈ 1:Y, mx ∈ 1:X
             for t ∈ 1:L
                 #n = mx + X*(my-1) + X*Y*(t-1)
+                wx = a*(-1+2*(mx-1)/(X-1))^b
+                wy = a*(-1+2*(my-1)/(Y-1))^b
                 n = t + L*(mx-1) + L*X*(my-1)
                 #println("mx=$mx, my=$my, t=$t, n=$n")
-                data[n] = SVector{$D*(2*$B + 1)^2, T}($(gens...))
+                data[n] = SVector{$D*(2*$B + 1)^2+2, T}($(gens...),wx, wy)
             end
         end
         data
     end
 end
 
-@generated function my_reconstruct(s, ::Val{D}, ::Val{B}, τ, boundary) where {D, B}
+@generated function my_reconstruct(s, ::Val{D}, ::Val{B}, τ, boundary, a, b) where {D, B}
     my_reconstruct_impl(Val{D}(), Val{B}())
 end
 
 function myReconstruction(s::AbstractArray{T,3}, D,τ::DT,B=1,k=1,boundary=10, a=1,b=1) where {T, DT}
-    Reconstruction{D*(2B+1)^2,T,DT}(my_reconstruct(s, Val{D}(), Val{B}(),τ,boundary), τ)
+    Reconstruction{D*(2B+1)^2+2,T,DT}(my_reconstruct(s, Val{D}(), Val{B}(),τ,boundary,a,b), τ)
 end
 
 
@@ -78,13 +79,27 @@ end
 #                                     Prediction                                          #
 ###########################################################################################
 
-
+function (M::AverageLocalModel)(q,xnn,ynn,dists)
+    if length(xnn) > 1 && maximum(dists) > 0
+        dmax = maximum(dists)
+        y_pred = zeros(typeof(ynn[1]))
+        Ω = 0.
+        for (y,d) in zip(ynn,dists)
+            ω2 = (1-0.9(d/dmax)^M.n)^2M.n
+            Ω += ω2
+            y_pred += ω2*y
+        end
+        y_pred /= Ω
+        return y_pred
+    end
+    return ynn[1]
+end
 
 function localmodel_stts(s,D,τ,p,B=1,k=1,boundary=20, a=1,b=1;
     method::AbstractLocalModel = AverageLocalModel(2),
     ntype::AbstractNeighborhood = FixedMassNeighborhood(3))
-    R = myReconstruction(s,D,τ,B,k,boundary)
-    #R = md_STReconstruction(s,D,τ,B,k,boundary)
+    #R = myReconstruction(s,D,τ,B,k,boundary)
+    R = md_STReconstruction(s,D,τ,B,k,boundary)
     #M = dimension(s)
     #L = length(s)
     X,Y,L = size(s)
@@ -116,8 +131,8 @@ function localmodel_stts(s,D,τ,p,B=1,k=1,boundary=20, a=1,b=1;
                     push!(q,s_pred[i,j,t])
                 end
             end
-            #push!(q, a*(-1+2*mx/X)^b)
-            #push!(q, a*(-1+2*my/Y)^b)
+            push!(q,a*(-1+2*(mx-1)/(X-1))^b)
+            push!(q,a*(-1+2*(mY-1)/(Y-1))^b)
             #make prediction & put into state
             idxs,dists = TimeseriesPrediction.neighborhood_and_distances(q,R,tree,ntype)
             xnn = R[idxs]
@@ -151,7 +166,7 @@ end
 X=10
 Y=10
 ds = coupled_henon(X,Y)
-N_train = 100
+N_train = 1000
 p = 30
 data = trajectory(ds,N_train+p)
 #Reconstruct this #
@@ -168,7 +183,7 @@ begin
     colorbar()
     subplot(312)
     #Prediction
-    s_pred = localmodel_stts(mat,3,1,p,1,1,10, 0,0)
+    s_pred = localmodel_stts(mat,2,1,p,1,1,10, 1,1)
     pred_mat = reshape(s_pred, (X*Y,p+1))'
     pcolormesh(pred_mat)
     colorbar()
--- src/barkley.jl
+++ src/barkley.jl
@@ -1,4 +1,3 @@
-using OrdinaryDiffEq
 using Plots
 #using PyPlot
 #pyplot()
@@ -9,18 +8,24 @@ using Plots
 
 # Simulation is super fast but plotting/animating sucks....
 
-function barkley(Nx=100, Ny=100)
+function barkley(T, Nx=100, Ny=100)
     a = 0.75
     b = 0.02
     ε = 0.02
-    T = 500
     V = zeros(Nx, Ny, T)
     u = zeros(Nx, Ny)
     v = zeros(Nx, Ny)
-    u[50:end,49] = 0.1
-    u[50:end,50] = 0.5
-    u[50:end,51] = 5
-    v[50:end,48] = 1
+    u[35:end,34] = 0.1
+    u[35:end,35] = 0.5
+    u[35:end,36] = 5
+    v[35:end,34] = 1
+
+    u[1:15,14] = 5
+    u[1:15,15] = 0.5
+    u[1:15,16] = 0.1
+    v[1:15,17] = 1
+
+
 
 
     #u[50:52, 47:48] = 1
@@ -66,4 +71,93 @@ function barkley(Nx=100, Ny=100)
     #    plot(@view(V[:,:,i]), st=[:contourf])
     #end every 10
     return V
+end
+
+Nx = 50
+Ny = 50
+Tskip = 20
+Ttrain = 50
+p = 100
+T = Tskip + Ttrain + p
+
+V = barkley(T, Nx, Ny)#[:,:,Tskip+1:T]
+Vtrain = V[:,:,Tskip + 1:Tskip + Ttrain]
+Vtest  = V[:,:,Tskip + Ttrain :  T]
+
+
+
+D = 2
+τ = 1
+B = 1
+k = 1
+a = 0
+b = 0
+boundary = 20
+
+
+function localmodel_stts(s,D,τ,p,B=1,k=1,boundary=20, a=1,b=1;
+    method::AbstractLocalModel = AverageLocalModel(2),
+    ntype::AbstractNeighborhood = FixedMassNeighborhood(3))
+    #R = myReconstruction(s,D,τ,B,k,boundary)
+    R = myReconstruction(s,D,τ,B,k,boundary, a, b)
+    #M = dimension(s)
+    #L = length(s)
+    X,Y,L = size(s)
+
+    #Prepare tree but remove the last reconstructed states first
+    R_tree = Dataset(copy(R.data))
+    for my=1:Y,mx=1:X
+        R_tree.data[(mx+X*(my-1))*(L-(D-1)*τ)] = SVector((Inf*ones(length(R[1])))...)
+    end
+    tree = KDTree(R_tree)
+
+
+    #Prepare s_pred with end of STTS so all initial queries can be created
+    s_pred = s[:,:,L-D*τ:L]
+
+
+    #New state that will be predicted, allocate once and reuse
+    state = zeros(X,Y)
+    q = zeros(length(R[1]))
+    for n=1:p
+        N = size(s_pred)[end]
+        for mx=1:X, my=1:Y
+            #create q from previous predictions
+            qidx = 1
+            for   t=N-D*τ+1:τ:N, j=my-B*k:k:my+B*k, i=mx-B*k:k:mx+B*k
+                if 0< i <=X && 0< j <=Y
+                    q[qidx] = s_pred[i,j,t]
+                else
+                    q[qidx] = boundary
+                end
+                qidx += 1
+            end
+            q[qidx]   = a*(-1+2*(mx-1)/(X-1))^b
+            q[qidx+1] = a*(-1+2*(my-1)/(Y-1))^b
+
+            #make prediction & put into state
+            idxs,dists = TimeseriesPrediction.neighborhood_and_distances(q,R,tree,ntype)
+            xnn = R[idxs]
+            ynn = map(y -> y[1+(2*B+1)*B+B+(D-1)*(2*B+1)^2],R[idxs+1])
+
+            state[mx,my] = method(q,xnn,ynn,dists)[1]
+        end
+        s_pred = cat(3,s_pred,state)
+    end
+    #return only the predictions without boundary and w/o old STTS
+    return s_pred[:,:,D*τ+1:end]
+end
+
+Vpred = localmodel_stts(Vtrain, D, τ, p, B, k, a, b)
+error = Vtest-Vpred
+ε = sum(error, (1,2))[:]
+#ε2 = sum(Vpred-Vpred2, (1,2))[:]
+
+@gif for i=1:size(Vtest)[3]
+    #l = @layout [a{0.5w} b]
+    p1 = plot(@view(Vtest[:,:,i]), st=[:contourf])
+    p2 = plot(@view(Vpred[:,:,i]), st=[:contourf])
+    #p3 = plot(@view(error[:,:,i]), st=[:contourf])
+    plot(p1,p2, layout=(1,2))#, size=(600,1000))
+    plot!(title = "barkley L=Orig, R=pred")
 end
