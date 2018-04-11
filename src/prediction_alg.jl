c45a3d1d524f4d972c552f71995403d42e39408d
Jonas Isensee <jonas.isensee@web.de>
2018-04-11 21:15:10 +0200

    Added 1D pred & function barrier

--- src/prediction_alg.jl
+++ src/prediction_alg.jl
@@ -6,25 +6,24 @@ using TimeseriesPrediction
 #                                     Prediction                                          #
 ###########################################################################################
 
-function localmodel_stts(s,D,τ,p,B=1,k=1,boundary=20, a=1,b=1;
+function localmodel_stts(s::AbstractArray{T,Ψ},D,τ,p,B=1,k=1,boundary=20, a=1,b=1;
     method::AbstractLocalModel = AverageLocalModel(2),
-    ntype::AbstractNeighborhood = FixedMassNeighborhood(3))
-
+    ntype::AbstractNeighborhood = FixedMassNeighborhood(3)) where {T, Ψ}
+    Φ = Ψ-1
     R = myReconstruction(s,D,τ,B,k,boundary, a, b)
-
-    #TODO: Add function barrier
-    _localmodel_sttts()
+    return _localmodel_stts(s, R, D, τ, p, B, k, boundary, a, b)
 end
-function _localmodel_stts(R::Reconstruction, D, B, τ, k) # FIX ME
+
+function _localmodel_stts(s::AbstractArray{T,3},R, D, τ, p, B, k, boundary, a, b;
+    method::AbstractLocalModel = AverageLocalModel(2),
+    ntype::AbstractNeighborhood = FixedMassNeighborhood(3)) where T
     X,Y,L = size(s)
 
     #Prepare tree but remove the last reconstructed states first
     tree = KDTree(R[1:end-X*Y])
 
-
     #Prepare s_pred with end of STTS so all initial queries can be created
     s_pred = s[:,:,L-D*τ:L]
-
 
     #New state that will be predicted, allocate once and reuse
     state = zeros(X,Y)
@@ -58,7 +57,47 @@ function _localmodel_stts(R::Reconstruction, D, B, τ, k) # FIX ME
     return s_pred[:,:,D*τ+1:end]
 end
 
+function _localmodel_stts(s::AbstractArray{T,2},R, D, τ, p, B, k, boundary, a, b;
+    method::AbstractLocalModel = AverageLocalModel(2),
+    ntype::AbstractNeighborhood = FixedMassNeighborhood(3)) where T
+    X,L = size(s)
 
+    #Prepare tree but remove the last reconstructed states first
+    tree = KDTree(R[1:end-X])
+
+    #Prepare s_pred with end of STTS so all initial queries can be created
+    s_pred = s[:,L-D*τ:L]
+
+    #New state that will be predicted, allocate once and reuse
+    state = zeros(X)
+    q = zeros(length(R[1]))
+    for n=1:p
+        N = size(s_pred)[end]
+        for mx=1:X
+            #create q from previous predictions
+            qidx = 1
+            for   t=N-D*τ+1:τ:N, i=mx-B*k:k:mx+B*k
+                if 0< i <=X
+                    q[qidx] = s_pred[i,t]
+                else
+                    q[qidx] = boundary
+                end
+                qidx += 1
+            end
+            q[qidx]   = a*(-1+2*(mx-1)/(X-1))^b
+
+            #make prediction & put into state
+            idxs,dists = TimeseriesPrediction.neighborhood_and_distances(q,R,tree,ntype)
+            xnn = R[idxs]
+            ynn = map(y -> y[1+B+(D-1)*(2*B+1)],R[idxs+X])
+
+            state[mx] = method(q,xnn,ynn,dists)[1]
+        end
+        s_pred = cat(2,s_pred,state)
+    end
+    #return only the predictions without boundary and w/o old STTS
+    return s_pred[:,D*τ+1:end]
+end
 
 
 
