54115a1d076ac1dbbe3ddf4c519a18e9e7aa6c5d
Jonas Isensee <jonas.isensee@web.de>
2018-04-12 12:06:21 +0200

    1D Cross-Pred

--- src/prediction_alg.jl
+++ src/prediction_alg.jl
@@ -102,9 +102,10 @@ end
 
 
 
-function crosspred_stts(strain,ytrain,spred,D,τ,B=1,k=1,boundary=20, a=1,b=1;
+function crosspred_stts(strain::AbstractArray{T,3}, ytrain, spred,
+    D,τ,B=1,k=1,boundary=20, a=1,b=1;
     method::AbstractLocalModel = AverageLocalModel(2),
-    ntype::AbstractNeighborhood = FixedMassNeighborhood(1))
+    ntype::AbstractNeighborhood = FixedMassNeighborhood(1)) where T
 
     R = myReconstruction(strain,D,τ,B,k,boundary, a, b)
     X,Y,L = size(strain)
@@ -139,6 +140,49 @@ function crosspred_stts(strain,ytrain,spred,D,τ,B=1,k=1,boundary=20, a=1,b=1;
             ynn = ytrain[idxs+X*Y*(D-1)]    #Indeces in R are shifted by X*Y rel. to ytrain
 
             ypred[mx,my,n] = method(q,xnn,ynn,dists)[1]
+        end
+    end
+    #return only the predictions without boundary and w/o old STTS
+    return ypred
+end
+
+function crosspred_stts(strain::AbstractArray{T,2}, ytrain, spred,
+    D,τ,B=1,k=1,boundary=20, a=1,b=1;
+    method::AbstractLocalModel = AverageLocalModel(2),
+    ntype::AbstractNeighborhood = FixedMassNeighborhood(3)) where T
+
+    R = myReconstruction(strain,D,τ,B,k,boundary, a, b)
+    X,L = size(strain)
+
+    #Prepare tree but remove the last reconstructed states first
+    tree = KDTree(R)
+
+
+    #Prepare s_pred with end of STTS so all initial queries can be created
+
+    q = zeros(length(R[1]))
+    N = size(spred)[end]
+    ypred = zeros((X,N-(D-1)*τ))
+    for n=1:N-(D-1)τ
+        for mx=1:X
+            #create q from previous predictions
+            qidx = 1
+            for   t=n:τ:n+(D-1)*τ, i=mx-B*k:k:mx+B*k
+                if 0< i <=X
+                    q[qidx] = spred[i,t]
+                else
+                    q[qidx] = boundary
+                end
+                qidx += 1
+            end
+            q[qidx]   = a*(-1+2*(mx-1)/(X-1))^b
+
+            #make prediction & put into state
+            idxs,dists = TimeseriesPrediction.neighborhood_and_distances(q,R,tree,ntype)
+            xnn = R[idxs]   #not used in method...
+            ynn = ytrain[idxs+X*(D-1)]    #Indeces in R are shifted by X*Y rel. to ytrain
+
+            ypred[mx,n] = method(q,xnn,ynn,dists)[1]
         end
     end
     #return only the predictions without boundary and w/o old STTS
