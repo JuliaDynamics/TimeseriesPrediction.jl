d17c08f3b26a8fea9470bdcfbb60be38162eeaea
Jonas Isensee <jonas.isensee@web.de>
2018-04-13 14:31:51 +0200

    Improved tspred algorithm - more code reused

--- src/prediction_alg.jl
+++ src/prediction_alg.jl
@@ -9,96 +9,61 @@ using TimeseriesPrediction
 function localmodel_stts(s::AbstractArray{T,Ψ},D,τ,p,B=1,k=1,boundary=20, a=1,b=1;
     method::AbstractLocalModel = AverageLocalModel(2),
     ntype::AbstractNeighborhood = FixedMassNeighborhood(3)) where {T, Ψ}
-    Φ = Ψ-1
+    Φ = Ψ-1  #Number of spatial Dimension
+    M = prod(size(s)[1:Φ])  #Total Number of spatial points
+    L = size(s)[Φ+1] #Number of temporal points
     R = myReconstruction(s,D,τ,B,k,boundary, a, b)
-    return _localmodel_stts(s, R, D, τ, p, B, k, boundary, a, b)
-end
-
-function _localmodel_stts(s::AbstractArray{T,3},R, D, τ, p, B, k, boundary, a, b;
-    method::AbstractLocalModel = AverageLocalModel(2),
-    ntype::AbstractNeighborhood = FixedMassNeighborhood(3)) where T
-    X,Y,L = size(s)
 
     #Prepare tree but remove the last reconstructed states first
-    tree = KDTree(R[1:end-X*Y])
+    tree = KDTree(R[1:end-M])
 
-    #Prepare s_pred with end of STTS so all initial queries can be created
-    s_pred = s[:,:,L-D*τ:L]
+    if Φ == 2
+        #Prepare s_pred with end of STTS so all initial queries can be created
+        s_pred = s[:,:,L-D*τ:L]
+        return _localmodel_stts(s_pred, R, tree, D, τ, p, B, k,
+         boundary, a, b)[:,:,D*τ+1:end]
+    elseif Φ == 1
+        s_pred = s[:,L-D*τ:L]
+        return _localmodel_stts(s_pred, R, tree, D, τ, p, B, k,
+         boundary, a, b)[:,D*τ+1:end]
+    end
+end
 
+function gen_qs(s_pred::AbstractArray{T, 3}, D, τ, B, k, boundary, a, b) where {T}
+    N = size(s_pred)[3]
+    s_slice = @view(s_pred[:,:,N-D*τ+1:τ:N])
+    return myReconstruction(s_slice, D, τ, B, k, boundary, a, b)
+end
+
+function gen_qs(s_pred::AbstractArray{T, 2}, D, τ, B, k, boundary, a, b) where {T}
+    N = size(s_pred)[2]
+    s_slice = @view(s_pred[:,N-D*τ+1:τ:N])
+    return myReconstruction(s_slice, D, τ, B, k, boundary, a, b)
+end
+
+function _localmodel_stts(s::AbstractArray{T,Ψ}, R, tree ,D, τ, p, B, k, boundary, a, b;
+    method::AbstractLocalModel = AverageLocalModel(2),
+    ntype::AbstractNeighborhood = FixedMassNeighborhood(3)) where {T, Ψ}
+    Φ = Ψ - 1
+    M = prod(size(s)[1:Φ])  #Total Number of spatial points
     #New state that will be predicted, allocate once and reuse
-    state = zeros(X,Y)
-    q = zeros(length(R[1]))
+    state = similar(Array{T}, indices(s)[1:Φ])
+    #Index of relevant element in ynn
+    im = 1 + (D-1)*(2B+1)^Φ + B*sum(i -> (2B+1)^(Φ-i), 1:Φ)
     for n=1:p
-        N = size(s_pred)[end]
-        for mx=1:X, my=1:Y
-            #create q from previous predictions
-            qidx = 1
-            for   t=N-D*τ+1:τ:N, j=my-B*k:k:my+B*k, i=mx-B*k:k:mx+B*k
-                if 0< i <=X && 0< j <=Y
-                    q[qidx] = s_pred[i,j,t]
-                else
-                    q[qidx] = boundary
-                end
-                qidx += 1
-            end
-            q[qidx]   = a*(-1+2*(mx-1)/(X-1))^b
-            q[qidx+1] = a*(-1+2*(my-1)/(Y-1))^b
-
+        qs = gen_qs(s, D, τ, B, k, boundary, a, b)
+        for (m,q) ∈ enumerate(qs)
             #make prediction & put into state
             idxs,dists = TimeseriesPrediction.neighborhood_and_distances(q,R,tree,ntype)
             xnn = R[idxs]
-            ynn = map(y -> y[1+(2*B+1)*B+B+(D-1)*(2*B+1)^2],R[idxs+X*Y])
+            ynn = map(y -> y[im],R[idxs+M])
 
-            state[mx,my] = method(q,xnn,ynn,dists)[1]
+            state[m] = method(q,xnn,ynn,dists)[1]
         end
-        s_pred = cat(3,s_pred,state)
+        s = cat(Φ+1,s,state)
     end
-    #return only the predictions without boundary and w/o old STTS
-    return s_pred[:,:,D*τ+1:end]
+    return s
 end
-
-function _localmodel_stts(s::AbstractArray{T,2},R, D, τ, p, B, k, boundary, a, b;
-    method::AbstractLocalModel = AverageLocalModel(2),
-    ntype::AbstractNeighborhood = FixedMassNeighborhood(3)) where T
-    X,L = size(s)
-
-    #Prepare tree but remove the last reconstructed states first
-    tree = KDTree(R[1:end-X])
-
-    #Prepare s_pred with end of STTS so all initial queries can be created
-    s_pred = s[:,L-D*τ:L]
-
-    #New state that will be predicted, allocate once and reuse
-    state = zeros(X)
-    q = zeros(length(R[1]))
-    for n=1:p
-        N = size(s_pred)[end]
-        for mx=1:X
-            #create q from previous predictions
-            qidx = 1
-            for   t=N-D*τ+1:τ:N, i=mx-B*k:k:mx+B*k
-                if 0< i <=X
-                    q[qidx] = s_pred[i,t]
-                else
-                    q[qidx] = boundary
-                end
-                qidx += 1
-            end
-            q[qidx]   = a*(-1+2*(mx-1)/(X-1))^b
-
-            #make prediction & put into state
-            idxs,dists = TimeseriesPrediction.neighborhood_and_distances(q,R,tree,ntype)
-            xnn = R[idxs]
-            ynn = map(y -> y[1+B+(D-1)*(2*B+1)],R[idxs+X])
-
-            state[mx] = method(q,xnn,ynn,dists)[1]
-        end
-        s_pred = cat(2,s_pred,state)
-    end
-    #return only the predictions without boundary and w/o old STTS
-    return s_pred[:,D*τ+1:end]
-end
-
 
 
 
