2fc885830cd5cc221bf0e039d37c5d9908293cc5
Datseris <datseris.george@gmail.com>
2018-04-30 23:34:31 +0200

    fix (?) 2D henon

--- examples/coupled_henon_tspred.jl
+++ examples/coupled_henon_tspred.jl
@@ -17,47 +17,51 @@ function coupled_henon2D(X=10,Y=10)
     return SpatioTemporalSystem(henon,rand(X,Y,2), nothing; t0=0)
 end
 
-
 #Size
-X=5
-Y=5
+X=7
+Y=7
 ds = coupled_henon2D(X,Y)
 N_train = 1000
-p = 25
+p = 50
 data = trajectory(ds,N_train+p)
-#Reconstruct this
-s = data[1:N_train,SVector(1:X*Y...)]
-s = map(state -> reshape([state...], X,Y), s)
 
-begin
-    ax1 = subplot(311)
-    #Original
-    img = Matrix(data[N_train:N_train+p,SVector(1:X*Y...)])
-    pcolormesh(img)
-    colorbar()
-    # Make x-tick labels invisible
-    setp(ax1[:get_xticklabels](), visible=false)
-    title("2D Coupled Henon Map")
+xdata = [d[:,:,1] for d in data]
 
-
-    #Prediction
-    ax2 = subplot(312, sharex = ax1, sharey = ax1)
-    s_pred = localmodel_stts(s,3,1,p,1,1) # ;weighting=(1,1)
-    pred =  [s_pred[t][i] for t=1:p+1,i=1:X*Y]
-    pcolormesh(pred)
-    colorbar()
-    setp(ax2[:get_xticklabels](), visible=false)
-    title("Prediction ( Training States $N_train)")
-    ylabel("t")
-
-    #Error
-    ax3 = subplot(313, sharex = ax1, sharey = ax1)
-    ε = abs.(img-pred)
-    pcolormesh(ε, cmap="inferno")
-    colorbar()
-    title("Absolute Error")
-    xlabel("i = (x, y)")
-    tight_layout()
-
-
+# make data 1D form, 2nd D to be time
+function makeinto1D(data)
+    img = zeros(p, X*Y)
+    for i in 1:p
+        for k in 1:X*Y
+            img[i, k] = data[i][k]
+        end
+    end
+    return img
 end
+
+ax1 = subplot(311)
+img = makeinto1D(xdata[N_train+1:end])
+pcolormesh(img)
+colorbar()
+# Make x-tick labels invisible
+setp(ax1[:get_xticklabels](), visible=false)
+title("2D Coupled Henon Map")
+
+
+#Prediction
+ax2 = subplot(312, sharex = ax1, sharey = ax1)
+s_pred = localmodel_stts(xdata[1:N_train],3,1,p,1,1) # ;weighting=(1,1)
+pred = makeinto1D(s_pred)
+pcolormesh(pred)
+colorbar()
+setp(ax2[:get_xticklabels](), visible=false)
+title("Prediction (Training $N_train)")
+ylabel("t")
+
+#Error
+ax3 = subplot(313, sharex = ax1, sharey = ax1)
+ε = abs.(img-pred)
+pcolormesh(ε, cmap="inferno")
+colorbar()
+title("Absolute Error")
+xlabel("i = (x, y)")
+tight_layout()
