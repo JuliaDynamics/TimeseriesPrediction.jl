40c15643dee8b104b3fe7d0231b097886a2fbfe2
Jonas Isensee <jonas.isensee@web.de>
2018-05-18 14:40:07 +0200

    x^2 → x^4

--- src/localmodeling.jl
+++ src/localmodeling.jl
@@ -17,7 +17,7 @@ have smaller impact on the prediction and so that the interpolation
 is smooth. The default weighting function we use is
 ```math
 \\begin{aligned}
-ω_i(d_i,d_{max}) = \\left[ 1- \\left(\\frac{d_i}{d_{max}}\\right)^2\\right]^2
+ω_i(d_i,d_{max}) = \\left[ 1- \\left(\\frac{d_i}{d_{max}}\\right)^2\\right]^4
 \\end{aligned}
 ```
 with ``d_i = ||x_{nn,i} -q||_2`` being the distance of each neighbor from
