75fee5152792a446c97312f84bc2a911446e0ac6
Jonas Isensee <jonas.isensee@web.de>
2018-08-05 18:09:54 +0200

    Tests for reconstructions

--- test/reconstruction_tests.jl
+++ test/reconstruction_tests.jl
@@ -1,21 +1,23 @@
 using Test
 using TimeseriesPrediction
+using PrincipalComponentAnalysis
 
 println("Reconstruction Tests")
 @testset "STDelayEmbedding" begin
-    for D=0:4, τ ∈ [1,10,20], B=1:3, k=1:3, c=10, Φ=1:4
+    for D=[0,4], τ ∈ [1,20], B=[1,3], k=[1,3], c=10, Φ=1:4
         @testset "D=$D, τ=$τ, B=$B, k=$k, Φ=$Φ" begin
-                emb = STDelayEmbedding(D,τ,B,k,c, Φ)
+                #Ugly way of creating Φ dim array
+                s = [rand(Float64,([10 for i=1:Φ]...,)) for i=1:10]
+                emb = STDelayEmbedding(s,D,τ,B,k,c)
                 #Check Embedding Dimension X
                 X = (D+1)*(2B+1)^Φ
-                @test typeof(emb) == STDelayEmbedding{X,Φ}
-                @test length(emb.delays) == X
-
-                #Check tmax
-                @test emb.tmax == D*τ
+                @test typeof(emb) <: STDelayEmbedding{Float64,Φ,X}
+                @test length(emb.τ) == X
+                @test length(emb.β) == X
 
                 #check spatial extent
-                @test emb.spex == B*k * ones(Φ,2)
+                @test emb.inner == CartesianIndices(([B*k+1:10-B*k for i=1:Φ]...,))
+                @test emb.outer == CartesianIndices(([1:10 for i=1:Φ]...,))
 
                 #boundary value
                 @test emb.boundary == c
@@ -26,14 +28,33 @@ println("Reconstruction Tests")
     @testset "Order of rec. points" begin
         D=1; τ=1; B=1; k=1; c=10; Φ=2
         data = [reshape(1+i:9+i, 3,3) for i∈[0,9]]
-        emb = STDelayEmbedding(D,τ,B,k,c,Φ)
-
-        pt_in_space = CartesianIndices((3,3))
+        emb = STDelayEmbedding(data, D,τ,B,k,c)
         t = 1
         α = CartesianIndex(2,2)
-        @test 1:18 == emb(data, t, α, pt_in_space)
+        @test 1:18 == emb(data, t, α)
         α = CartesianIndex(1,1)
         rec = [c,c,c,c,1,2,c,4,5,c,c,c,c,10,11,c,13,14]
-        @test rec == emb(data, t, α, pt_in_space)
+        @test rec == emb(data, t, α)
+    end
+end
+
+
+include("system_defs.jl")
+
+println("Testing PCA Functions")
+@testset "PCAEmbedding" begin
+    U,V = barkley_periodic_boundary_nonlin(500,50,50)
+    for D=10, τ=1, B=1,k=1, c=false, Φ=2
+        em = STDelayEmbedding(U,D,τ,B,k,c)
+        R = TimeseriesPrediction.reconstruct(U, em)
+        Rmat = Matrix(R)
+        drmodel = fit(PCA,Rmat)
+        pcaem = PCAEmbedding(U,em)
+        #Compare Outdim
+        @test outdim(drmodel) == outdim(pcaem.drmodel)
+        #Compare singular values
+        @test isapprox(principalvars(drmodel), principalvars(pcaem.drmodel), atol=1e-2)
+         #Compare Projection Matrices
+        @test isapprox(drmodel.proj, pcaem.drmodel.proj, atol=1e-2)
     end
 end
