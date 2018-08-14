05a8fd0ec1237ab859023ab5fb55ba06a179277b
Jonas Isensee <jonas.isensee@web.de>
2018-08-14 17:33:36 +0200

    Adapt tests to BoundaryCondition change

--- test/reconstruction_tests.jl
+++ test/reconstruction_tests.jl
@@ -1,17 +1,19 @@
 using Test
 using TimeseriesPrediction
 using PrincipalComponentAnalysis
+using Statistics, LinearAlgebra
 
 println("Reconstruction Tests")
 @testset "STDelayEmbedding" begin
-    for D=[0,4], τ ∈ [1,20], B=[1,3], k=[1,3], c=10, Φ=1:4
+    for D=[0,4], τ ∈ [1,20], B=[1,3], k=[1,3], Φ=1:4
+        BC=ConstantBoundary{10}
         @testset "D=$D, τ=$τ, B=$B, k=$k, Φ=$Φ" begin
                 #Ugly way of creating Φ dim array
                 s = [rand(Float64,([10 for i=1:Φ]...,)) for i=1:10]
-                emb = STDelayEmbedding(s,D,τ,B,k,c)
+                emb = STDelayEmbedding(s,D,τ,B,k,BC)
                 #Check Embedding Dimension X
                 X = (D+1)*(2B+1)^Φ
-                @test typeof(emb) <: STDelayEmbedding{Float64,Φ,X}
+                @test typeof(emb) <: STDelayEmbedding{Float64,Φ,BC,X}
                 @test length(emb.τ) == X
                 @test length(emb.β) == X
 
@@ -20,18 +22,15 @@ println("Reconstruction Tests")
                                 ([B*k+1 for i=1:Φ]...,),
                                 ([10-B*k for i=1:Φ]...,))
 
-                @test emb.outer == TimeseriesPrediction.Region((ones(Int,Φ)...,),(10ones(Int,Φ)...,) )
-
-                #boundary value
-                @test emb.boundary == c
+                @test emb.whole == TimeseriesPrediction.Region((ones(Int,Φ)...,),(10ones(Int,Φ)...,) )
         end
     end
 
     #Checking a few individual Reconstructions
     @testset "Order of rec. points" begin
-        D=1; τ=1; B=1; k=1; c=10; Φ=2
+        D=1; τ=1; B=1; k=1; c=10; BC=ConstantBoundary{c}; Φ=2
         data = [reshape(1+i:9+i, 3,3) for i∈[0,9]]
-        emb = STDelayEmbedding(data, D,τ,B,k,c)
+        emb = STDelayEmbedding(data, D,τ,B,k,BC)
         t = 1
         α = CartesianIndex(2,2)
         rvec = zeros(18);
@@ -50,17 +49,21 @@ include("system_defs.jl")
 println("Testing PCA Functions")
 @testset "PCAEmbedding" begin
     U,V = barkley_periodic_boundary_nonlin(500,50,50)
-    for D=10, τ=1, B=1,k=1, c=false, Φ=2
-        em = STDelayEmbedding(U,D,τ,B,k,c)
-        R = DynamicalSystemsBase.reconstruct(U, em)
-        Rmat = Matrix(R)
-        drmodel = fit(PCA,Rmat)
+    for D=5, τ=1, B=1,k=1
+        BC = PeriodicBoundary
+        em = STDelayEmbedding(U,D,τ,B,k,BC)
+        R = TimeseriesPrediction.reconstruct(U, em)
+        meanv = mean(mean.(U))
+        R .-= meanv
+        covmat = Statistics.covzm(R,2)
         pcaem = PCAEmbedding(U,em)
+        #Compare cov matrices
+        @test norm(pcaem.covmat-covmat) < 1e-2
         #Compare Outdim
+        drmodel = fit(PCA,R; mean=0)
         @test outdim(drmodel) == outdim(pcaem.drmodel)
         #Compare singular values
-        @test isapprox(principalvars(drmodel), principalvars(pcaem.drmodel), atol=5e-2)
-         #Compare Projection Matrices
-        @test isapprox(drmodel.proj, pcaem.drmodel.proj, atol=5e-2)
+        @test all(isapprox.(principalvars(drmodel), principalvars(pcaem.drmodel), atol=0.1))
+
     end
 end
--- test/st_pred_barkley.jl
+++ test/st_pred_barkley.jl
@@ -16,12 +16,12 @@ include("system_defs.jl")
 
     τ = 1
     k = 1
-    c = 20
+    BC = ConstantBoundary{20}
 
     @testset "V, D=$D, B=$B" for D=2, B=1
         Vtrain = V[Tskip + 1:Tskip + Ttrain]
         Vtest  = V[Tskip + Ttrain :  T]
-        em = STDelayEmbedding(Vtrain,D,τ,B,k,c)
+        em = STDelayEmbedding(Vtrain,D,τ,B,k,BC)
         Vpred = localmodel_stts(Vtrain,em,p).spred
         @test Vpred[1] == Vtrain[end]
         err = [abs.(Vtest[i]-Vpred[i]) for i=1:p+1]
@@ -33,7 +33,7 @@ include("system_defs.jl")
         D=2; B=1
         Utrain = U[Tskip + 1:Tskip + Ttrain]
         Utest  = U[Tskip + Ttrain :  T]
-        em = STDelayEmbedding(Utrain,D,τ,B,k,c)
+        em = STDelayEmbedding(Utrain,D,τ,B,k,BC)
         Upred = localmodel_stts(Utrain,em,p).spred
         @test Upred[1] == Utrain[end]
         err = [abs.(Utest[i]-Upred[i]) for i=1:p+1]
@@ -47,7 +47,7 @@ include("system_defs.jl")
         Vtrain = V[Tskip + 1:Tskip + Ttrain]
         Utest  = U[Tskip + Ttrain - (D-1)τ + 1:  T]
         Vtest  = V[Tskip + Ttrain - (D-1)τ + 1:  T]
-        em = STDelayEmbedding(Vtrain, D,τ,B,k,c)
+        em = STDelayEmbedding(Vtrain, D,τ,B,k,BC)
         R = reconstruct(Vtrain, em)
         tree = KDTree(R[:,1:end-2500])
         Upred = crosspred_stts(Utrain,Vtest, em, R, tree).pred_out
@@ -64,7 +64,7 @@ include("system_defs.jl")
         c = false
         Vtrain = V[Tskip + 1:Tskip + Ttrain]
         Vtest  = V[Tskip + Ttrain :  T]
-        em = STDelayEmbedding(Vtrain, D,τ,B,k,c)
+        em = STDelayEmbedding(Vtrain, D,τ,B,k,BC)
         Vpred = localmodel_stts(Vtrain, em, p).spred
         @test Vpred[1] == Vtrain[end]
         err = [abs.(Vtest[i]-Vpred[i]) for i=1:p+1]
@@ -77,10 +77,10 @@ include("system_defs.jl")
         Ttrain = 500
         T = Tskip + Ttrain + p
         U,V = barkley_periodic_boundary_nonlin(T, Nx, Ny)
-        c = false
+        BC = PeriodicBoundary
         Vtrain = V[Tskip + 1:Tskip + Ttrain]
         Vtest  = V[Tskip + Ttrain :  T]
-        em = STDelayEmbedding(Vtrain, D,τ,B,k,c)
+        em = STDelayEmbedding(Vtrain, D,τ,B,k,BC)
         Vpred = localmodel_stts(Vtrain, em, p).spred
         @test Vpred[1] == Vtrain[end]
         err = [abs.(Vtest[i]-Vpred[i]) for i=1:p+1]
