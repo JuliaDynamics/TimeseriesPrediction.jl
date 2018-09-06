36c1126c50d5c3c4ea0ea497b27c0f9f277264db
Jonas Isensee <jonas.isensee@web.de>
2018-09-06 10:21:07 +0200

    Added Docstrings for Temporal- and CrossPrediction

--- src/crossprediction.jl
+++ src/crossprediction.jl
@@ -19,20 +19,58 @@ mutable struct CrossPrediction{T,Φ,BC,X}
                                 Dict{Symbol,Float64}(), Array{T,Φ}[])
 end
 
+"""
+    CrossPrediction(source_train, target_train, source_pred,
+                   em::AbstractSpatialEmbedding; kwargs...)
+Perform a spatio-temporal timeseries cross-prediction for `target` from
+`source`, using local weighted modeling [1]. This can be used for example
+when there are coupled spatial fields and one is used to predict the other.
+It is assumed that `source_train`, `target_train`, `source_pred` are all of
+the same type, `AbstractVector{<:AbstractArray{T, Φ}}`.
+
+The spatio temporal delay embedding process is defined by `em`.
+See [`AbstractSpatialEmbedding`](@ref) for available methods and interfaces.
+
+## Keyword Arguments
+  * `ttype = KDTree` : Type/Constructor of tree structure. So far only tested with `KDTree`.
+  * `method = AverageLocalModel(ω_safe)` : Subtype of [`AbstractLocalModel`](@ref).
+  * `ntype = FixedMassNeighborhood(3)` : Subtype of [`AbstractNeighborhood`](@ref).
+  * `printprogress = true` : To print progress done.
+## Description
+The reconstructed state of `source_train[t][i,j,...]` is associated with the output
+value `target_train[t][i,j,...]`. This establishes a "connection"
+between `target` and `source`.
+Taking a reconstructed state of `source_pred` as query point,
+the function finds its neighbors in the reconstructed space of `source_train` using
+neighborhood `ntype`. Then, the neighbor *indices* are used
+to make a prediction for the corresponding value of the `target`, using the
+established "connection" between fields (from the training data).
+The algorithm is applied for all points in space and time of `pred_in` minus the first
+states that are needed for reconstruction.
+## Additional Interfaces
+To save computation time in the case of repeated predictions with the same training set
+and embedding parameters we provide an additional interface that allows the user to
+provide an existing reconstruction and tree structure.
+    R = reconstruct(train_in, em)
+    tree = ttype(R)
+    prelim_sol = CrossPrediction(em, method, ntype, ttype, pred_in)
+    sol = CrossPrediction(copy(prelim_sol), train_out, R, tree; progress=true)
+where `prelim_sol` is a preliminary solution struct containing all relevant parameters.
+
+## Performance Notes
+Be careful when choosing embedding parameters as memory usage and computation time
+depend strongly on the resulting embedding dimension.
+## References
+[1] : U. Parlitz & C. Merkwirth, [Phys. Rev. Lett. **84**, pp 1890 (2000)](https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.84.1890)
+"""
 function CrossPrediction(train_in ::AbstractVector{<:AbstractArray{T, Φ}},
                         train_out::AbstractVector{<:AbstractArray{T, Φ}},
                         pred_in  ::AbstractVector{<:AbstractArray{T, Φ}},
-                        em::AbstractSpatialEmbedding{T,Φ,BC,X};
-                        treetype = KDTree,
                         em::AbstractSpatialEmbedding{T,Φ};
                         ttype = KDTree,
                         method::AbstractLocalModel  = AverageLocalModel(ω_safe),
                         ntype::AbstractNeighborhood = FixedMassNeighborhood(3),
                         progress=true
-                        ) where {T, Φ, BC, X}
-    sol = CrossPrediction{T,Φ,BC,X}(em, method, ntype, treetype, pred_in,
-                                    Dict{Symbol,Float64}(), Array{T,Φ}[])
-    CrossPrediction(sol, train_in, train_out; progress=true)
                         ) where {T, Φ}
     prelim_sol = CrossPrediction(em, method, ntype, ttype, pred_in)
     CrossPrediction(prelim_sol, train_in, train_out; progress=true)
--- src/temporalprediction.jl
+++ src/temporalprediction.jl
@@ -49,7 +49,39 @@ mutable struct TemporalPrediction{T,Φ,BC,X}
 end
 
 
+"""
+    TemporalPrediction(U, em::AbstractSpatialEmbedding, tsteps; kwargs...)
+Perform a spatio-temporal time series prediction for `tsteps` iterations,
+using local weighted modeling [1] give a time series of the form
+`U::AbstractVector{<:AbstractArray{T, Φ}}`. The function returns a
+solution struct with the prediction `spred` of the same type as `U`.
 
+The returned data always contains the final state of `U` as starting point
+(total returned length is `tsteps+1`).
+The reconstruction process is defined by `em`.
+For available methods and interfaces see [`AbstractSpatialEmbedding`](@ref)
+## Keyword Arguments
+  * `ttype = KDTree` : Type/Constructor of tree structure. So far only tested with `KDTree`.
+  * `method = AverageLocalModel(ω_safe)` : Subtype of [`AbstractLocalModel`](@ref).
+  * `ntype = FixedMassNeighborhood(3)` : Subtype of [`AbstractNeighborhood`](@ref).
+  * `printprogress = true` : To print progress done.
+## Description
+This method works similarly to [`localmodel_tsp`](@ref), by expanding the concept
+of delay embedding to spatially extended systems. Instead of reconstructing
+complete states of the system, local states are used. This implicitely assumes
+a finite speed `c` at which information travels within the system as well as a
+sufficiently fine spatial and temporal sampling such that
+``\\frac{\\Delta x}{\\Delta t}\\sim c``.
+See [`AbstractSpatialEmbeddingn`](@ref) for details on the embedding.
+Predictions are then performed frame by frame and point py point. Once all values for a new
+frame are found, the frame is added to the end of the timeseries and used to generate
+new prediction queries for the next time step.
+## Performance Notes
+Be careful when choosing embedding parameters as memory usage and computation time
+depend strongly on the resulting embedding dimension.
+## References
+[1] : U. Parlitz & C. Merkwirth, [Phys. Rev. Lett. **84**, pp 1890 (2000)](https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.84.1890)
+"""
 function TemporalPrediction(s,
     em::AbstractSpatialEmbedding{T,Φ},
     tsteps;
