df4108eefb6f1e9e60de8fa7a9e12e67c7ed04d9
Jonas Isensee <jonas.isensee@web.de>
2018-09-17 15:49:00 +0200

    undo PCA embedding change

--- src/pcareconstruction.jl
+++ src/pcareconstruction.jl
@@ -19,8 +19,8 @@ Takes a [`SpatioTemporalEmbedding`](@ref) and computes PCA on the time series `s
 
 To set the output dimension to a certain value `X`, pass `pratio=1, maxoutdim=X`.
 """
-struct PCAEmbedding{T,Φ,BC,X,Y,EM<:AbstractSpatialEmbedding} <: AbstractSpatialEmbedding{T,Φ,BC,X}
-	stem::EM{T,Φ,BC,Y}
+struct PCAEmbedding{T,Φ,BC,X,Y} <: AbstractSpatialEmbedding{T,Φ,BC,X}
+	stem::SpatioTemporalEmbedding{T,Φ,BC,Y}
 	meanv::T
 	covmat::Matrix{T}
 	drmodel::PCA{T}
