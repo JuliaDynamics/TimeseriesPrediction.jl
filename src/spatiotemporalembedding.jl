191a01d31018e4b1f07325c7ce3578a27a5f8933
George Datseris <datseris.george@gmail.com>
2018-09-21 14:13:29 +0200

    add plot light cone

--- src/spatiotemporalembedding.jl
+++ src/spatiotemporalembedding.jl
@@ -293,3 +293,42 @@ SpatioTemporalEmbedding(s, p::NamedTuple{(:N, :τ, :r₀, :c, :bc)}) =
 
 Base.:(==)(em1::T, em2::T) where {T <: AbstractSpatialEmbedding} =
     all(( eval(:($em1.$name == $em2.$name)) for name ∈ fieldnames(T)))
+
+
+
+"""
+    explain_light_cone(;D = 2, τ = 2, r₀ = 1, c = 1)
+Requires `PyPlot`.
+"""
+function explain_light_cone(;D = 2, τ = 2, r0 = 1, c = 1)
+
+    maxr = D*τ*c + r0
+
+    figure()
+    xticks(-maxr:maxr)
+    yticks(-maxr:maxr)
+
+    for i in D:-1:0
+        r = i*τ*c + r0
+        points = indices_within(r, 2)
+        radius = maximum(norm(Tuple(p)) for p in points)
+
+        if r != 0
+            x = r*cos.(range(0, stop = 2π, length = 100))
+            y = r*sin.(range(0, stop = 2π, length = 100))
+            plot(x,y, c = "C$i")
+        end
+
+        x = map(xy -> xy[1], points)
+        y = map(xy -> xy[2], points)
+        scatter(x,y, c = "C$i", s=100, zorder = 3, label = "within \$r = r_0 + $i \\tau c\$")
+
+    end
+    title("D = $D, τ = $τ, r₀ = $r0, c = $c")
+    PyPlot.grid(zorder = -1)
+    legend(loc = "upper right")
+    xlabel("x")
+    ylabel("y")
+    PyPlot.axis("equal")
+    tight_layout()
+end
