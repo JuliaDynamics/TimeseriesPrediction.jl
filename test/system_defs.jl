de218c3a2a60ad6d1ee011a58a29c7f87b6ff5ef
JonasIsensee <jonas.isensee@web.de>
2018-09-24 14:03:42 +0200

    Betterbarkley (tests) (#47)
    * Better barkley implementation
    
    * make barkley a little more compact
    
    * missed one call to old barkley function
    
    * removed params NamedTuple
    
    * Fix BUG in example script + switch to new barkley

--- test/system_defs.jl
+++ test/system_defs.jl
@@ -45,42 +45,25 @@ function coupled_henon2D(X,Y,N,u0=rand(X,Y), v0=rand(X,Y))
 
 end
 
-
-function barkley_const_boundary(T, Nx, Ny)
+function barkley(T;
+                tskip=0,
+                periodic=true,
+                size=(50,50),
+                a=0.75, b=0.06, ε=0.08, D=1/50, h=0.1, Δt=0.1)
+    Nx, Ny = size
     @assert Nx ≥ 40
-    a = 0.75
-    b = 0.02
-    ε = 0.02
-
-    u = zeros(Nx, Ny)
-    v = zeros(Nx, Ny)
+    @assert Ny ≥ 40
     U = Vector{Array{Float64,2}}()
     V = Vector{Array{Float64,2}}()
 
-    #Initial state that creates spirals
-    u[40:end,34] .= 0.1
-    u[40:end,35] .= 0.5
-    u[40:end,36] .= 5
-    v[40:end,34] .= 1
-
-    u[1:10,14] .= 5
-    u[1:10,15] .= 0.5
-    u[1:10,16] .= 0.1
-    v[1:10,17] .= 1
-
-    u[27:36,20] .= 5
-    u[27:36,19] .= 0.5
-    u[27:36,18] .= 0.1
-    v[27:36,17] .= 1
-
-
-
-    h = 0.75 #/ sqrt(2)
-    Δt = 0.1 #/ 2
-    δ = 0.001
+    u = zeros(Nx,Ny)
+    v = zeros(Nx,Ny)
+    init = rand(10,10)
+    v .= u .= repeat(init, inner=(Nx÷10,Ny÷10))
+    v .= v.>0.2
     Σ = zeros(Nx, Ny, 2)
-    r = 1
-    s = 2
+    r,s = 1,2
+
     function F(u, uth)
         if u < uth
             u/(1-(Δt/ε)*(1-u)*(u-uth))
@@ -89,170 +72,40 @@ function barkley_const_boundary(T, Nx, Ny)
         end
     end
 
-    for m=1:T
+    function periodic_step()
+        for j=1:Ny, i=1:Nx
+            uth = (v[i,j] + b)/a
+            v[i,j] = v[i,j] + Δt*(u[i,j]^3 - v[i,j])
+            u[i,j] = F(u[i,j], uth) + D * Δt/h^2 *Σ[i,j,r]
+            Σ[i,j,s] -= 4u[i,j]
+            Σ[  mod(i-1-1,Nx)+1,j,s] += u[i,j]
+            Σ[  mod(i+1-1,Nx)+1,j,s] += u[i,j]
+            Σ[i,mod(j-1-1,Ny)+1,  s] += u[i,j]
+            Σ[i,mod(j+1-1,Ny)+1,  s] += u[i,j]
+            Σ[i,j,r] = 0
+        end
+    end
+    function constant_step()
         for i=1:Nx, j=1:Ny
-            if u[i,j] < δ
-                u[i,j] = Δt/h^2 * Σ[i,j,r]
-                v[i,j] = (1 - Δt)* v[i,j]
-            else
-                uth = (v[i,j] + b)/a
-                v[i,j] = v[i,j] + Δt*(u[i,j] - v[i,j])
-                u[i,j] = F(u[i,j], uth) + Δt/h^2 *Σ[i,j,r]
-                Σ[i,j,s] -= 4u[i,j]
-                i > 1  && (Σ[i-1,j,s] += u[i,j])
-                i < Nx && (Σ[i+1,j,s] += u[i,j])
-                j > 1  && (Σ[i,j-1,s] += u[i,j])
-                j < Ny && (Σ[i,j+1,s] += u[i,j])
-            end
+            uth = (v[i,j] + b)/a
+            v[i,j] = v[i,j] + Δt*(u[i,j] - v[i,j])
+            u[i,j] = F(u[i,j], uth) + Δt/h^2 *Σ[i,j,r]
+            Σ[i,j,s] -= 4u[i,j]
+            i > 1  && (Σ[i-1,j,s] += u[i,j])
+            i < Nx && (Σ[i+1,j,s] += u[i,j])
+            j > 1  && (Σ[i,j-1,s] += u[i,j])
+            j < Ny && (Σ[i,j+1,s] += u[i,j])
             Σ[i,j,r] = 0
         end
+    end
+
+    for m=1:T+tskip
+        periodic ? periodic_step() : constant_step()
         r,s = s,r
-        #V[:,:,m] .= v
-        #U[:,:,m] .= u
-        push!(U,copy(u))
-        push!(V,copy(v))
-    end
-    return U,V
-end
-
-function barkley_periodic_boundary(T, Nx, Ny)
-    a = 0.75
-    b = 0.02
-    ε = 0.02
-
-    u = zeros(Nx, Ny)
-    v = zeros(Nx, Ny)
-    U = Vector{Array{Float64,2}}()
-    V = Vector{Array{Float64,2}}()
-
-    #Initial state that creates spirals
-    u[40:end,34] .= 0.1
-    u[40:end,35] .= 0.5
-    u[40:end,36] .= 5
-    v[40:end,34] .= 1
-
-    u[1:10,14] .= 5
-    u[1:10,15] .= 0.5
-    u[1:10,16] .= 0.1
-    v[1:10,17] .= 1
-
-    u[27:36,20] .= 5
-    u[27:36,19] .= 0.5
-    u[27:36,18] .= 0.1
-    v[27:36,17] .= 1
-
-    h = 0.75 #/ sqrt(2)
-    Δt = 0.1 #/ 2
-    δ = 0.01
-    Σ = zeros(Nx, Ny, 2)
-    r = 1
-    s = 2
-    function F(u, uth)
-        if u < uth
-            u/(1-(Δt/ε)*(1-u)*(u-uth))
-        else
-            (u + (Δt/ε)*u*(u-uth))/(1+(Δt/ε)*u*(u-uth))
+        if m > tskip
+            push!(U,copy(u))
+            push!(V,copy(v))
         end
-    end
-
-    for m=1:T
-        for j=1:Ny, i=1:Nx
-            if u[i,j] < δ
-                u[i,j] = Δt/h^2 * Σ[i,j,r]
-                v[i,j] = (1 - Δt)* v[i,j]
-            else
-                uth = (v[i,j] + b)/a
-                v[i,j] = v[i,j] + Δt*(u[i,j] - v[i,j])
-                u[i,j] = F(u[i,j], uth) + Δt/h^2 *Σ[i,j,r]
-                Σ[i,j,s] -= 4u[i,j]
-                Σ[  mod(i-1-1,Nx)+1,j,s] += u[i,j]
-                Σ[  mod(i+1-1,Nx)+1,j,s] += u[i,j]
-                Σ[i,mod(j-1-1,Ny)+1,  s] += u[i,j]
-                Σ[i,mod(j+1-1,Ny)+1,  s] += u[i,j]
-            end
-            Σ[i,j,r] = 0
-        end
-        r,s = s,r
-        #V[:,:,m] .= v
-        #U[:,:,m] .= u
-        push!(U,copy(u))
-        push!(V,copy(v))
-    end
-    return U,V
-end
-
-
-function barkley_periodic_boundary_nonlin(T, Nx, Ny)
-    a = 0.75
-    b = 0.02
-    ε = 0.02
-
-    u = zeros(Nx, Ny)
-    v = zeros(Nx, Ny)
-    U = Vector{Array{Float64,2}}()
-    V = Vector{Array{Float64,2}}()
-
-    #Initial state that creates spirals
-    u[35:end,34] .= 1
-    u[35:end,35] .= 1
-    u[35:end,36] .= 1
-    v[35:end,37] .= 1
-    v[35:end,38] .= 1
-    v[35:end,39] .= 1
-
-
-    u[1:20,14] .= 1
-    u[1:20,15] .= 1
-    u[1:20,16] .= 1
-    v[1:20,17] .= 1
-    v[1:20,18] .= 1
-    v[1:20,19] .= 1
-    v[1:20,20] .= 1
-
-
-    u[27:36,20] .= 1
-    u[27:36,19] .= 1
-    u[27:36,18] .= 1
-    v[27:36,17] .= 1
-    v[27:36,16] .= 1
-    v[27:36,15] .= 1
-
-    h = 0.75 #/ sqrt(2)
-    Δt = 0.1 #/ 2
-    δ = 0.001
-    Σ = zeros(Nx, Ny, 2)
-    r = 1
-    s = 2
-    function F(u, uth)
-        if u < uth
-            u/(1-(Δt/ε)*(1-u)*(u-uth))
-        else
-            (u + (Δt/ε)*u*(u-uth))/(1+(Δt/ε)*u*(u-uth))
-        end
-    end
-
-    for m=1:T
-        for j=1:Ny, i=1:Nx
-            if u[i,j] < δ
-                u[i,j] = Δt/h^2 * Σ[i,j,r]
-                v[i,j] = (1 - Δt)* v[i,j]
-            else
-                uth = (v[i,j] + b)/a
-                v[i,j] = v[i,j] + Δt*(u[i,j]^3 - v[i,j])
-                u[i,j] = F(u[i,j], uth) + Δt/h^2 *Σ[i,j,r]
-                Σ[i,j,s] -= 4u[i,j]
-                Σ[  mod(i-1-1,Nx)+1,j,s] += u[i,j]
-                Σ[  mod(i+1-1,Nx)+1,j,s] += u[i,j]
-                Σ[i,mod(j-1-1,Ny)+1,  s] += u[i,j]
-                Σ[i,mod(j+1-1,Ny)+1,  s] += u[i,j]
-            end
-            Σ[i,j,r] = 0
-        end
-        r,s = s,r
-        #V[:,:,m] .= v
-        #U[:,:,m] .= u
-        push!(U,copy(u))
-        push!(V,copy(v))
     end
     return U,V
 end
