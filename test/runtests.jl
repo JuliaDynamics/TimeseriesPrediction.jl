17f3f34ad179cf0c972c6b53273075394fef8442
JonasIsensee <jonas.isensee@web.de>
2019-03-07 12:03:48 +0100

    Add new test file to runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,6 +10,8 @@ include("localmodeling_tests.jl")
 include("st_pred_barkley.jl")
 include("st_coupledhenon_tests.jl")
 include("reconstruction_tests.jl")
+include("inhomogeneity_tests.jl")
+include("symmetry_tests.jl")
 ti = time() - ti
 println("\nTest took total time of:")
 println(round(ti, digits=3), " seconds or ", round(ti/60, digits=3), " minutes")
