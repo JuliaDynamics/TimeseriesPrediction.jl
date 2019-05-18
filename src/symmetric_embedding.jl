c20efefabb0cd8427db690220e844e4d77fa8e4d
George Datseris <datseris.george@gmail.com>
2019-05-18 17:12:44 +0200

    a note on the definition of the symmetries

--- src/symmetric_embedding.jl
+++ src/symmetric_embedding.jl
@@ -7,6 +7,9 @@ export Reflection, Rotation, Symmetry
 Supertype of all symmetries used in [`SymmetricEmbedding`](@ref).
 All symmetries are initialized like `Symmetry(n1, n2, ...)` with
 `ni` being the indices of the spatial dimensions that have the said symmetry.
+
+Notice that the symmetries are defined with respect to the center point
+of the embedding.
 """
 abstract type Symmetry end
 
